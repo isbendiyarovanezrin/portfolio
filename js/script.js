bb5987dcfe6afa8e11626b9f1e38ff516ed96909
Nəzrin <isbndyrva@gmail.com>
2022-02-28 16:33:38 +0400

    Add loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 // background music
 const song = document.getElementById("song");
 song.play();
+
+// loader
+$(window).on("load", () => {
+  setTimeout(removeLoader, 1800);
+});
+
+function removeLoader() {
+  $("#loadingDiv").fadeOut(400, () => {
+    $("#loadingDiv").remove();
+  });
+}
